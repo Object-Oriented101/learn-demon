c61081a16a24a8d85633bd4ceb102c1904ea9bef
Prajeet Sangamnerkar <reality@Prajeets-MacBook-Pro.local>
2022-04-02 17:20:23 -0500

    Added Project Scope to project

--- plotly_proj/views.py
+++ plotly_proj/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from projects.models import Progress_Block,Project, Investor
+from projects.models import Progress_Block, Project
 import pandas as pd
 from plotly.offline import plot
 import plotly.express as px
@@ -7,6 +7,8 @@ import random
 
 
 def index(request):
+
+    #Line Graph Code
     progress_blocks = Progress_Block.objects.all()
     parsed_progress_blocks = [
         {
@@ -20,51 +22,10 @@ def index(request):
     line_fig = px.line(df_progress_block, x = "Date", y = "Time", title = "Daily Progress")
     line_plot = plot(line_fig, output_type="div")
 
-    #Database Scatter Plot
-    investors = Investor.objects.all()
-    investors_data = [
-        {
-            'Name': x.name,
-            'Age': x.age,
-            'College': x.college,
-            'Net_Worth': x.net_worth
-
-        } for x in investors
-    ]
-    df_investors_data = pd.DataFrame(investors_data)
-    scatter_fig= px.scatter(df_investors_data, x = "Age", y = "Net_Worth", text = "Name")
-    scatter_fig.update_traces(textposition='top center')
-    scatter_plot = plot(scatter_fig, output_type="div")
-
-
-    #Bar Graph
-    qs = Project.objects.all()
-    projects_data = [
-        {
-            'Project': x.name,
-            'Start': x.start_date,
-            'Finish': x.end_date,
-            'Responsible': x.responsible.username
-        } for x in qs
-    ]
-
-    df = pd.DataFrame(projects_data)
-
-    fig = px.timeline(
-        df, x_start="Start", x_end="Finish", y="Project", color="Responsible"
-    )
-
-    fig.update_yaxes(autorange="reversed") 
-    gantt_plot = plot(fig, output_type="div")
-
-    #Box Plot
-    random.seed(10)
-    randomlist = []
-    for i in range(0,100):
-        n = random.randint(1,100)
-        randomlist.append(n)
+    #Project Details passback
+    project_retrieval = Project.objects.all() #ERROR CHECK IF EMPTY
+    project_details = project_retrieval[0]
     
-    box_plot = plot(px.box(randomlist), output_type="div")
 
-    context = {'line_plot': line_plot, 'gantt_plot': gantt_plot, 'scatter_plot': scatter_plot, 'box_plot': box_plot}
+    context = {'line_plot': line_plot, 'project_details': project_details}
     return render(request, 'index.html', context)
--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Progress_Block, Project, Investor
+from .models import Progress_Block, Project
 
 # Register your models here.
 admin.site.register(Project)
-admin.site.register(Investor)
 admin.site.register(Progress_Block)
